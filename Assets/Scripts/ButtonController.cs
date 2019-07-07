6146ed5ed97bf98309369138b5d5301053dcbd74
Dennis Hiller <mail@dennishiller.de>
2019-07-07 22:19:37 +0200

    Designanpassung Quiz

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject ReturnButton;
+    
+    public void CameraScene()
+    {
+        SceneManager.LoadScene("All-Scenes");
+    }
+}
