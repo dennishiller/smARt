e5d6568f886d4b147f96fee47f9b8834fded5725
Dennis Hiller <mail@dennishiller.de>
2019-07-07 19:48:50 +0200

    some Quiz changes

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using MySql.Data;
+
+public class Database : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
