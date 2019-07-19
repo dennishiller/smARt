a211dfa2c6652bcd4a430f7eec643ee152bffb21
aimazeme <50926329+aimazeme@users.noreply.github.com>
2019-07-19 14:52:32 +0200

    doxygen shit

--- Assets/Scripts/MusicMManager.cs
+++ Assets/Scripts/MusicMManager.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * @class Music Manager
+ * 
+ * @brief This script manages audio across all scenes
+ * 
+ * This script is saved in a prefab game object which is used to play the given 
+ * audio across all scenes with the set volume. So whenever a scene is switch it 
+ * plays from the beginning and the set volume from the options menu will not be 
+ * deleted but saved.
+ */
 public class MusicMManager : MonoBehaviour
 {
+    /// <summary>
+    /// Audiosource object refering to audio clip
+    /// </summary>
     public AudioSource music;
+    
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1f;
         music.volume = PlayerPrefs.GetFloat("VolumeSlider");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/SoundMenu.cs
+++ Assets/Scripts/SoundMenu.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * @class Sound Menu
+ * 
+ *@brief Script in order to play sound in setted volume, which is also connected to volume slider
+ * 
+ */
 public class SoundMenu : MonoBehaviour
 {
     public AudioSource music;
